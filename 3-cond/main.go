a67b0b951971f99df5eec70cf8043cb0db1d2e49
parsa sedigh <p.sedigh@wallex.net>
2026-08-14 21:34:08 +0330

    refactor: make cond example better

--- 3-cond/main.go
+++ 3-cond/main.go
@@ -1,66 +1,76 @@
 package main
 
 import (
-	"math/rand"
+	"errors"
 	"sync"
-	"time"
 )
 
 var (
-	pokemonList = []string{"Pikachu", "Charmander", "Squirtle", "Bulbasaur", "Jigglypuff"}
-
-	// shard state that is mutated
-	pokemon = ""
-	mu      = &sync.Mutex{}
-	cond    = sync.NewCond(mu)
+	ErrClosed = errors.New("queue is closed")
 )
 
-func main() {
-	var wg sync.WaitGroup
+// queue is bounded queue
+type queue struct {
+	mu       sync.Mutex
+	notEmpty *sync.Cond // we wanna wait for when the queue is NOT empty. Map this requirement to fields exactly
+	notFull  *sync.Cond
 
-	wg.Add(2)
+	items       []int
+	maxItemsNum int
+	closed      bool
+}
 
-	// Consumer(waiter goroutine)
-	go func() {
-		defer wg.Done()
+func newQueue(maxItemsNum int) *queue {
+	q := &queue{maxItemsNum: maxItemsNum}
+	q.notEmpty = sync.NewCond(&q.mu)
+	q.notFull = sync.NewCond(&q.mu)
 
-		cond.L.Lock()
-		defer cond.L.Unlock()
+	return q
+}
 
-		/* When woken, rechecks the condition (loop ensures this).
+func (q *queue) put(item int) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
 
-		Q: Why use a for loop when checking the condition?
-		A:
-		*/
-		for pokemon != "Pikachu" {
-			cond.Wait()
-		}
+	if len(q.items) == q.maxItemsNum && !q.closed {
+		q.notFull.Wait()
+	}
+	if q.closed {
+		return ErrClosed
+	}
 
-		// Now Condition is definitely true, since this goroutine has exclusive access to the mutex of cond
+	q.items = append(q.items, item)
+	q.notEmpty.Signal() // added exactly one item, so wake exactly one getter
 
-		println("Caught" + pokemon)
-		pokemon = ""
-	}()
+	return nil
+}
 
-	// producer(signaler goroutine)
-	go func() {
-		defer wg.Done()
+func (q *queue) get() (int, error) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
 
-		// Every 1ms, a random Pokémon appears
+	if len(q.items) == 0 && !q.closed {
+		q.notEmpty.Wait()
+	}
+	if q.closed {
+		return 0, ErrClosed
+	}
 
-		time.Sleep(1 * time.Millisecond)
+	v := q.items[0]
+	q.items = q.items[1:]
 
-		for i := 0; i < 100; i++ {
-			cond.L.Lock()
-			pokemon = pokemonList[rand.Intn(len(pokemonList))] // Update shared state
+	q.notFull.Signal() // freed exactly one slot, maybe some goroutine is waiting for this, so wake exactly one putter
 
-			/* Note: Don't call Signal() after unlocking the mutex. It will create a small window where another goroutine could
-			modify pokemon after the producer sets it but before it signals. So a third sneaky goroutine can’t change pokemon until producer unlocks,
-			ensuring the signal corresponds to the state. */
-			cond.Signal() // Wake first consumer goroutine in the queue
-			cond.L.Unlock()
-		}
-	}()
+	return v, nil
+}
 
-	wg.Wait()
+func (q *queue) close() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.closed = true
+
+	// ALL goroutines should recheck and leave
+	q.notEmpty.Broadcast()
+	q.notFull.Broadcast()
 }
